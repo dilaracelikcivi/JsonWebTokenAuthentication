a0a940a7549dc6cad00b137f296835f371236a97
Dilara Çelikçivi <dilaracelikcivi@gmail.com>
2020-09-10 14:03:08 +0300

    For generated token information, new view model has been created.

---
+++ JsonWebToken.Authentication/Model/TokenWithClaimsPrincipal.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+
+namespace JsonWebToken.Authentication.Model
+{
+    public class TokenWithClaimsPrincipal
+    {
+        public string AccessToken { get; internal set; }
+        public ClaimsPrincipal ClaimsPrincipal { get; internal set; }
+        public AuthenticationProperties AuthenticationProperties { get; internal set; }
+    }
+}
