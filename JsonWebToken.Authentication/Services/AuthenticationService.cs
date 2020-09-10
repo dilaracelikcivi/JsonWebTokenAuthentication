07721d82d60f5e36381943b3e83fae65f02fd759
Dilara Çelikçivi <dilaracelikcivi@gmail.com>
2020-09-10 14:14:38 +0300

    Authentication and cookie information have been configured.

---
+++ JsonWebToken.Authentication/Services/AuthenticationService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JsonWebToken.Authentication.
+    Services
+{
+    public static class AuthenticationService
+    {
+        public static IServiceCollection AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    options.DefaultSignOutScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                })
+                .AddCookie(options =>
+                {
+                    options.LoginPath = "/Account/Login";
+                    options.LogoutPath = "/Account/Logout";
+                    options.AccessDeniedPath = options.LoginPath;
+                    options.ReturnUrlParameter = "returnUrl";
+                    options.Cookie.Name = "";
+                    options.ExpireTimeSpan = TimeSpan.FromMinutes(120);
+                });
+
+            return services;
+        }
+    }
+}
