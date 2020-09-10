6e8a471d1628054af29e2b4502da0a6859674a10
Dilara Çelikçivi <dilaracelikcivi@gmail.com>
2020-09-10 14:07:03 +0300

    Jwt token generator method signatures have been defined in an interface.

---
+++ JsonWebToken.Authentication/Data/IJwtTokenGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using JsonWebToken.Authentication.Model;
+
+namespace JsonWebToken.Authentication.Data
+{
+    public interface IJwtTokenGenerator
+    {
+        // Use this method for ASP.NET Core web application with cookie authentication.
+        TokenWithClaimsPrincipal GenerateAccessTokenWithClaimsPrincipal(IEnumerable<Claim> userClaims);
+
+        // Use this method for ASP.NET Core Web API applications with cookieless authentication.
+        string GenerateAccessToken(IEnumerable<Claim> userClaims, string issuer, string audience, string secret,
+            string algorithm, int expirationTime);
+    }
+}
