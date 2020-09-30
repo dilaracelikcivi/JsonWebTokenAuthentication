3d0f6819f149290f2fce83fe6bb1ea503dc9a957
Dilara Çelikçivi <dilaracelikcivi@gmail.com>
2020-09-30 15:26:23 +0300

    Model validation extension has been created to validate model and get validation messages.

---
+++ JsonWebToken.Authentication/Extensions/ModelValidatorExtension.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace JsonWebToken.Authentication.Extensions
+{
+    public static class ModelValidatorExtension
+    {
+        /// <summary>
+        /// This method provides to validate model for class
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public static bool IsValid(this object model)
+        {
+            var validationContext = new ValidationContext(model);
+            return Validator.TryValidateObject(model, validationContext, null);
+        }
+        
+        /// <summary>
+        /// This method provides to get validation result by model
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public static string ValidationResult(this object model)
+        {
+            var validationResult = new List<ValidationResult>();
+            var validationContext = new ValidationContext(model);
+
+            Validator.TryValidateObject(model, validationContext, validationResult);
+            
+            if (model is IValidatableObject) 
+                (model as IValidatableObject).Validate(validationContext);    
+                
+            return JsonConvert.SerializeObject(validationResult.Select(a => a.ErrorMessage));    
+        }
+    }
+}
