b930866eee8437d8f2b4ea433e28b7b31b161f93
Dilara Çelikçivi <dilaracelikcivi@gmail.com>
2020-09-30 15:25:33 +0300

    Json Web token generator method signatures have been updated from parameters to model.

--- JsonWebToken.Authentication/Data/IJwtTokenGenerator.cs
+++ JsonWebToken.Authentication/Data/IJwtTokenGenerator.cs
@@ -1,16 +1,13 @@
-﻿using System.Collections.Generic;
-using System.Security.Claims;
-using JsonWebToken.Authentication.Model.Token;
+﻿using JsonWebToken.Authentication.Model;
 
 namespace JsonWebToken.Authentication.Data
 {
     public interface IJwtTokenGenerator
     {
         // Use this method for ASP.NET Core web application with cookie authentication.
-        TokenWithClaimsPrincipal GenerateAccessTokenWithClaimsPrincipal(IEnumerable<Claim> userClaims);
+        TokenWithClaimsPrincipal GenerateAccessTokenWithClaimsPrincipal(AccessTokenToClaimsModel model);
 
         // Use this method for ASP.NET Core Web API applications with cookieless authentication.
-        string GenerateAccessToken(IEnumerable<Claim> userClaims, string issuer, string audience, string secret,
-            string algorithm, int expirationTime);
+        string GenerateAccessToken(AccessTokenModel model);
     }
 }
