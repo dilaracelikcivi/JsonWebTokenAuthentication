60b018b3ff34a28bc8588f732a7cc0edf149ba62
Dilara Çelikçivi <dilaracelikcivi@gmail.com>
2020-09-30 15:23:35 +0300

    Access token models have been created for jwt generator methods.

---
+++ JsonWebToken.Authentication/Model/AccessTokenModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JsonWebToken.Authentication.Model
+{
+    public class AccessTokenModel
+    {
+        public AccessTokenModel()
+        {
+            
+        }
+        public AccessTokenModel(string secret, IEnumerable<Claim> userClaims)
+        {
+            Secret = secret;
+            UserClaims = userClaims;
+        }
+        
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        
+        [Required]
+        public string Secret { get; set; }
+
+        [Required]
+        public IEnumerable<Claim> UserClaims { get; set; }
+        
+        public int ExpirationTime { get; set; } = 120;
+        public string Algorithm { get; set; } = SecurityAlgorithms.HmacSha256Signature;
+    }
+}
---
+++ JsonWebToken.Authentication/Model/AccessTokenToClaimsModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+
+namespace JsonWebToken.Authentication.Model
+{
+    public class AccessTokenToClaimsModel
+    {
+        [Required] public string Secret { get; set; }
+        [Required] public IEnumerable<Claim> UserClaims { get; set; }
+    }
+}
