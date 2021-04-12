4b320286503c126bde8f36cebd78d51e59d6abff
Dilara Çelikçivi <dilaracelikcivi@gmail.com>
2021-04-12 13:48:20 +0300

    For authentication options, new constructors have been added.

--- JsonWebToken.Authentication/Model/ServiceConfiguration/AuthenticationOptions.cs
+++ JsonWebToken.Authentication/Model/ServiceConfiguration/AuthenticationOptions.cs
@@ -4,6 +4,19 @@ namespace JsonWebToken.Authentication.Model.ServiceConfiguration
 {
     public class AuthenticationOptions
     {
+        public AuthenticationOptions()
+        {
+        }
+        
+        public AuthenticationOptions(string defaultAuthenticateScheme, string defaultChallengeScheme, 
+            string defaultSignInScheme, string defaultSignOutScheme)
+        {
+            DefaultAuthenticateScheme = defaultAuthenticateScheme;
+            DefaultChallengeScheme = defaultChallengeScheme;
+            DefaultSignInScheme = defaultSignInScheme;
+            DefaultSignOutScheme = defaultSignOutScheme;
+        }
+        
         public string DefaultAuthenticateScheme { get; set; } = CookieAuthenticationDefaults.AuthenticationScheme;
         public string DefaultChallengeScheme { get; set; } = CookieAuthenticationDefaults.AuthenticationScheme;
         public string DefaultSignInScheme { get; set; } = CookieAuthenticationDefaults.AuthenticationScheme;
